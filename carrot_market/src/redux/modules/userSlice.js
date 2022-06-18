45790e956626a7ef4fb94d93558a00a67455729a
junsu0121 <sayber123@naver.com>
2022-06-18 11:58:18 +0900

    로그인 토큰 문제 해결 #10

--- carrot_market/src/redux/modules/userSlice.js
+++ carrot_market/src/redux/modules/userSlice.js
@@ -1,29 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import instance from '../../shared/axios'
+import instance from "../../shared/axios";
 //미들웨어
 //login
-export const loginUserDB = ( users, close ) => {
+export const loginUserDB = (users) => {
   return async function (dispatch) {
-    console.log('나는 유저스',users)
     await instance
       //서버에 데이터 값 넣기
       // .post("http://54.180.86.234:8080/user/login", users)
       .post("/user/login", users)
       .then((response) => {
-        console.log(response)
-        const accessToken = response.data.token;
+        const accessToken = response.data.accessToken;
         //서버에서 받은 토큰 저장
         localStorage.setItem("token", accessToken);
         // 저장된 토큰으로 login 여부 확인
-        // if (accessToken) {
-        //   dispatch(loginUser(true));
-        //   close();
-        // }
+        if (accessToken) {
+          dispatch(loginUser(true));
+        }
+        window.location.reload();
       })
       .catch(function (error) {
         // 로그인 실패 시 에러메시지
-        console.log(error)
+        console.log(error);
         window.alert(error.response.data.errorMessage);
       });
   };
