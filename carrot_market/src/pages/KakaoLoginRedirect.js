0ef49b7c5c51464b79b69e5a27dbd96795f924fa
junsu0121 <sayber123@naver.com>
2022-06-21 19:12:06 +0900

    에러페이지, 컨텐츠 검색결과 없을 때 페이지 생성 #46

--- carrot_market/src/pages/KakaoLoginRedirect.js
+++ carrot_market/src/pages/KakaoLoginRedirect.js
@@ -1,35 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const KakaoLoginRedirect = () => {
-    
+  useEffect(() => {
+    // 토큰 get
+    const accessToken = new URL(window.location.href).searchParams.get(
+      "Authorization"
+    );
+    console.log(accessToken);
+    localStorage.setItem("token", accessToken);
 
-    useEffect(() => {
-        // 토큰 get
-        const accessToken = new URL(window.location.href).searchParams.get(
-            "Authorization"
-          );
-          console.log(accessToken);
-          localStorage.setItem("token", accessToken);
-          
-          //id get
-          const username = new URL(window.location.href).searchParams.get("username");
-          console.log(username);
-          localStorage.setItem("username", username);
-          
-          //닉네임 get
-          const nickname = new URL(window.location.href).searchParams.get("nickname");
-          console.log(nickname);
-          localStorage.setItem("nickname", nickname);
-          
-          //profile 이미지 get
-          const profileImage = new URL(window.location.href).searchParams.get("profile");
-          console.log(profileImage);
-          localStorage.setItem("profileImage", profileImage);
-        window.location.replace("/");
-      }, []);
-    return <></>
+    //id get
+    const username = new URL(window.location.href).searchParams.get("username");
+    console.log(username);
+    localStorage.setItem("username", username);
+
+    //닉네임 get
+    const nickname = new URL(window.location.href).searchParams.get("nickname");
+    const decodeName = decodeURI(decodeURIComponent(nickname));
+    console.log(decodeName);
+    localStorage.setItem("nickname", decodeName);
+
+    //profile 이미지 get
+    const profileImage = new URL(window.location.href).searchParams.get(
+      "profile"
+    );
+    console.log(profileImage);
+    const decodeProfileImage = decodeURI(decodeURIComponent(profileImage));
+    console.log(decodeProfileImage);
+    localStorage.setItem("profileImage", decodeProfileImage);
+
+    //메인으로 보내기
+    window.location.replace("/");
+  }, []);
+  return <></>;
 };
-
-
 
 export default KakaoLoginRedirect;
